eb56bcee8fb6842e2f8377581282cbc8874e8538
Riankai1073 <riancorci@gmail.com>
2024-11-17 03:49:38 -0600

    fixed _app

---
+++ vroom/app/_app.tsx
@@ -0,0 +1,16 @@
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import '@/styles/globals.css';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#000000" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
