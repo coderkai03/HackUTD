5a6eed25f3d47557e4d1e5812cc535291d19cc63
Riankai1073 <riancorci@gmail.com>
2024-11-17 04:03:04 -0600

    moved driver to home

--- vroom/app/page.tsx
+++ vroom/app/page.tsx
@@ -1,11 +1,11 @@
 import GoogleMap from "@/components/GoogleMap";
 import Image from "next/image";
+import Driver from "./driver/page";
 
 export default function Home() {
   return (
     <div>
-      hi
-      {/* <GoogleMap/> */}
+      <Driver/>
     </div>
   );
 }
